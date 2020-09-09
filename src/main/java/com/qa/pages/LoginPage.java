37c348fccfab35da1883e18aebe7e6c6f200033a
CodeGym7 <swathibh16@gmail.com>
2020-09-09 10:36:28 +0200

    Modifies LoginPage

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -5,6 +5,10 @@ public class LoginPage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Login Page");
+
+	System.out.println("Added one line");
 	}
+	
+	
 
 }
